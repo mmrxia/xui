bdeec91ee6bc70be7fcc63db8d5fb1c803936e2d
mrxia <xqslove@yeah.net>
2016-04-12 17:47:37 +0800

    update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,30 @@
  * gulp 打包
  * */
 
-var gulp = require('gulp'), //删除清空文件夹
+var gulp = require('gulp'),
     gutil = require('gulp-util'),
     del = require('del'),  //删除清空文件夹
     rename = require('gulp-rename'),    //重命名
     concat = require('gulp-concat'),    //合并
     jshint = require('gulp-jshint'),    //js校验
     uglify = require('gulp-uglify'),    //js压缩
-    minifyCss = require('gulp-minify-css'),     //css压缩
-    revAppend = require('gulp-rev-append'),  //用于生成版本号
-    rev = require('gulp-rev'),  //用于生成版本号
-    revCollector = require('gulp-rev-collector');     //gulp-rev的插件，用户更改html引用路径
+    minifyCss = require('gulp-minify-css');     //css压缩
 
 // Task: js concat and uglify
 gulp.task('js', function () {
     return gulp.src(['./examples/js/*.js'])
-        .pipe(concat('xui.min.js'))
+        .pipe(concat('xui.js', {newLine: ';'}))
+        .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest('./dist'));
 });
 
 // Task: css concat and uglify
 gulp.task('css', function () {
-    return gulp.src(['./examples/css/*.css'])
+    return gulp.src(['./examples/css/ui.css', './examples/css/*.css'])
         .pipe(concat('xui.css'))
-        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifyCss())
         .pipe(gulp.dest('./dist'));
 });
 
